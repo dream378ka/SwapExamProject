a05464c917f7c9840022bf0513a7f2177ebdb8dd
dream378ka <dream378ka@gmail.com>
2020-04-04 12:43:55 +0700

    no message

--- src/main/java/com/scb/exam/springboot/SpringbootApplication.java
+++ src/main/java/com/scb/exam/springboot/SpringbootApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class SpringbootApplication extends SpringBootServletInitializer {
@@ -15,5 +17,13 @@ public class SpringbootApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootApplication.class, args);
 	}
+	
+}
 
+@RestController
+class HelloController {
+	@GetMapping("/")
+	String hello() {
+		return "hello word";
+	}
 }
